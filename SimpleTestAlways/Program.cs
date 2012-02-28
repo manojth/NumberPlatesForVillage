a7af85ae2d90a41bba27fdbacd6bd6ed06fbf269
Manoj Tharayil <manoj.tharayil@gmail.com>
2012-02-28 15:18:28 +0530

    Using recursion to find the answer.

---
+++ SimpleTestAlways/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleTestAlways
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find the number of zeroes in number plates from 1 to 1000");
+            int houseNumberStartingFrom = 1;
+            int houseNumberEndingat = 111;
+
+
+            int totalNumberOfZeroes = 0;
+            
+
+            for (int i = houseNumberStartingFrom; i <= houseNumberEndingat; i++)
+            {
+                totalNumberOfZeroes = totalNumberOfZeroes + FindZeroesInSingleNumber(i, 0, 0);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Number of zeroes: "+totalNumberOfZeroes);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        private static int FindZeroesInSingleNumber(int number,int count,int position)
+        {
+            position = number.ToString().IndexOf('0', position);
+            if (position < 0)
+            {
+                return count;
+            }
+            count++;
+            position++;
+            return FindZeroesInSingleNumber(number,count, position);
+        }
+    }
+}
